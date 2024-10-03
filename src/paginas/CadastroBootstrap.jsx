c8fe7c8eff44c6ec8de7fff7bf9f750393734bcd
GabrielPBuck <gabriel.buck@hotmail.com>
2024-10-02 21:17:39 -0300

    Atividade01-Bootstrap-Tailwind

--- src/paginas/CadastroBootstrap.jsx
+++ src/paginas/CadastroBootstrap.jsx
@@ -13,51 +13,66 @@ const CadastroBootstrap = () => {
       <div className="posts">
         <article className="cartao">
           <Form className="p-0">
-            <Form.Group controlId="formId" className="mb-3">
-              <Form.Label>ID</Form.Label>
-              <Form.Control type="text" placeholder="Digite o ID" />
-            </Form.Group>
-
-            <Form.Group controlId="formNome" className="mb-3">
-              <Form.Label>Nome</Form.Label>
-              <Form.Control type="text" placeholder="Digite seu nome" />
-            </Form.Group>
-
-            <Form.Group controlId="formDataNascimento" className="mb-3">
-              <Form.Label>Data de Nascimento</Form.Label>
-              <Form.Control type="date" />
-            </Form.Group>
-
-            <Form.Group controlId="formCEP" className="mb-3">
-              <Form.Label>CEP</Form.Label>
-              <Form.Control type="text" placeholder="Digite o CEP" />
-            </Form.Group>
-
-            <Form.Group controlId="formUF" className="mb-3">
-              <Form.Label>UF</Form.Label>
-              <Form.Control type="text" placeholder="Digite o estado" />
-            </Form.Group>
-
-            <Form.Group controlId="formCidade" className="mb-3">
-              <Form.Label>Cidade</Form.Label>
-              <Form.Control type="text" placeholder="Digite a cidade" />
-            </Form.Group>
-
-            <Form.Group controlId="formBairro" className="mb-3">
-              <Form.Label>Bairro</Form.Label>
-              <Form.Control type="text" placeholder="Digite o bairro" />
-            </Form.Group>
-
-            <Form.Group controlId="formRua" className="mb-3">
-              <Form.Label>Rua</Form.Label>
-              <Form.Control type="text" placeholder="Digite a rua" />
-            </Form.Group>
-
-            <Form.Group controlId="formNumero" className="mb-3">
-              <Form.Label>Número</Form.Label>
-              <Form.Control type="text" placeholder="Digite o número" />
-            </Form.Group>
-
+            <div className="row">
+              <div className="col-3">
+                <Form.Group controlId="formId" className="mb-3">
+                  <Form.Label>ID</Form.Label>
+                  <Form.Control type="text" placeholder="Digite o ID" />
+                </Form.Group>
+              </div>
+              <div className="col-5">
+                <Form.Group controlId="formNome" className="mb-3">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type="text" placeholder="Digite seu nome" />
+                </Form.Group>
+              </div>
+              <div className="col-4">
+                <Form.Group controlId="formDataNascimento" className="mb-3">
+                  <Form.Label>Data de Nascimento</Form.Label>
+                  <Form.Control type="date" />
+                </Form.Group>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3">
+                <Form.Group controlId="formCEP" className="mb-3">
+                  <Form.Label>CEP</Form.Label>
+                  <Form.Control type="text" placeholder="Digite o CEP" />
+                </Form.Group>
+              </div>
+              <div className="col-6">
+                <Form.Group controlId="formCidade" className="mb-3">
+                  <Form.Label>Cidade</Form.Label>
+                  <Form.Control type="text" placeholder="Digite a cidade" />
+                </Form.Group>
+              </div>
+              <div className="col-3">
+                <Form.Group controlId="formUF" className="mb-3">
+                  <Form.Label>UF</Form.Label>
+                  <Form.Control type="text" placeholder="Digite o estado" />
+                </Form.Group>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-5">
+                <Form.Group controlId="formRua" className="mb-3">
+                  <Form.Label>Rua</Form.Label>
+                  <Form.Control type="text" placeholder="Digite a rua" />
+                </Form.Group>
+              </div>
+              <div className="col-4">
+                <Form.Group controlId="formBairro" className="mb-3">
+                  <Form.Label>Bairro</Form.Label>
+                  <Form.Control type="text" placeholder="Digite o bairro" />
+                </Form.Group>
+              </div>
+              <div className="col-3">
+                <Form.Group controlId="formNumero" className="mb-3">
+                  <Form.Label>Número</Form.Label>
+                  <Form.Control type="text" placeholder="Digite o n.º" />
+                </Form.Group>
+              </div>
+            </div>
             <Button variant="primary" type="submit" className="mb-1">
               Cadastrar
             </Button>
